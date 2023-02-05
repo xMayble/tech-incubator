5bfc7dcbc0ba8ca4aed4b27aecdcff7eabd37e40
Mehbub Rohit <mehburohit@gmail.com>
2023-02-05 03:34:35 -0500

    changes to view and status of task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,52 +1,77 @@
 import React, { useState } from 'react';
-import { json, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Task.css';
 
 function Task() {
 
-    const [tasks, setTasks] = useState(
-        JSON.parse(localStorage.getItem('tasks')) || []
-    );
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  );
 
-    const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
-    const handleAddTask = () => {
-        setTasks([...tasks, task]);
-        localStorage.setItem('tasks', JSON.stringify([...tasks, task]));
-    }
+  const [taskStatus, setTaskStatus] = useState(
+    JSON.parse(localStorage.getItem('taskStatus')) || {}
+  );
 
-    const handleTaskChange = (e) => {
-        setTask(e.currentTarget.value);
-    }
+  const handleAddTask = () => {
+    setTasks([...tasks, task]);
+    localStorage.setItem('tasks', JSON.stringify([...tasks, task]));
 
-    const handleDeleteTask = (index) => {
-        const newTasks = [...tasks];
-        newTasks.splice(index, 1);
-        setTasks(newTasks);
-        localStorage.setItem('tasks', JSON.stringify(newTasks));
-    }
+     // Set the default task status for the new task
+    setTaskStatus({ ...taskStatus, [tasks.length]: '' });
+    localStorage.setItem(
+    'taskStatus',
+    JSON.stringify({ ...taskStatus, [tasks.length]: '' })
+  );
+};
   
-    const taskList = tasks.map((t, index) =>
-    <div key={index}>
-      <Link to={`/task/${index}`}>
-        <span className='task-item'>{t}</span>
+
+  const handleTaskChange = (e) => {
+    setTask(e.currentTarget.value);
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+    localStorage.setItem('tasks', JSON.stringify(newTasks));
+  };
+
+  const handleStartTask = (index) => {
+    setTaskStatus({ ...taskStatus, [index]: "In Progress" });
+    localStorage.setItem(
+      'taskStatus',
+      JSON.stringify({ ...taskStatus, [index]: "In Progress" })
+    );
+  };
+
+  
+
+  const taskList = tasks.map((t, index) => (
+    <div className="task-item" key={index}>
+      <span>{t}</span>
+      <span>{taskStatus[index] ? `*${taskStatus[index]}*` : ""}</span>
+      <Link to={`/task/${index}`} onClick={() => handleStartTask(index)}>
+        <button>View Task</button>
       </Link>
       <button onClick={() => handleDeleteTask(index)}>Delete</button>
     </div>
-  );
-
-    return (
-        <div className="task-container">
-            <div>
-                Enter Task: <input value={task} onChange={handleTaskChange}/>
-                <br />
-                <button onClick={handleAddTask}>Add Task</button>
-            </div>
-
-            <ul>{taskList}</ul>
-
-        </div>
-    );
-  }
+  ));
   
-  export default Task;
+
+   return (
+    <div className="task-container">
+      <div>
+        Enter Task: <input value={task} onChange={handleTaskChange} />
+        <br />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+
+      <ul>{taskList}</ul>
+    </div>
+  );
+   };
+
+export default Task;
+
